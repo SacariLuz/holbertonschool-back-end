63e59f7dd0709f6f18f45192123e7006a852eeb2
SacariLuz <asacaricachi@gmail.com>
2024-02-04 19:41:57 -0800

    script to export data in the CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,42 +1,37 @@
 #!/usr/bin/python3
-"""
-Script that, for a given employee ID, returns information about their TODO
-list progress exported as a csv file
-"""
-
+"""Consumimos API para extraer información ficticia"""
 import csv
 import requests
 from sys import argv
 
+
 def main():
-    """
-    Query name and tasks of employee.
-    """
+    """Consultamos el nombre y las tareas de un empleado."""
     if len(argv) >= 2 and argv[1].isdigit():
         id = argv[1]
 
-    url_todos = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
-    url_id = f"https://jsonplaceholder.typicode.com/users/{id}"
+        url_id = f"https://jsonplaceholder.typicode.com/users/{id}"
+        url_todos = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
 
-    response = requests.get(url_id)
+        response = requests.get(url_id)
 
         if response.status_code != 200:
-            print(f"Error:username not found")
+            print(f"Ups... tuvimos un problema par consultar el {id}")
             exit()
 
         data = response.json()
-        EMPLOYEE_NAME = data["username"]
+        EMPLOYEE_NAME = data['username']
 
         response = requests.get(url_todos)
 
         if response.status_code != 200:
-            print(f"Error:username not found")
+            print(f"Ups... tuvimos un problema par consultar el {id}")
             exit()
 
         todos = response.json()
 
-        all_tasks = [todo["title"] for todo in todos]
-        status_task = [todo["completed"] for todo in todos]
+        all_tasks = [todo['title'] for todo in todos]
+        status_task = [todo['completed'] for todo in todos]
         employee_todos = []
 
         for index in range(0, len(all_tasks)):
@@ -45,14 +40,14 @@ def main():
 
             employee_todos.append(record)
 
-        name_file_csv = f"{id}.csv"
+        name_file_csv = f'{id}.csv'
 
-        with open(name_file_csv, mode="w", newline="") as f:
+        with open(name_file_csv, mode='w', newline='') as f:
             writer = csv.writer(f, quoting=csv.QUOTE_ALL)
             writer.writerows(employee_todos)
+    else:
+        print("Se esperaba que ingresará un ID valido")
 
-        else:
-            print("Please enter an existing user id")
 
-    if __name__ == "__main__":
-        main()
+if __name__ == '__main__':
+    main()
