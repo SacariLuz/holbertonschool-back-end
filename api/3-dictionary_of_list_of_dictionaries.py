15e9d11bf8198e4568ed8970e14f5bcad6cd9524
SacariLuz <asacaricachi@gmail.com>
2024-02-06 07:36:28 -0800

    correction PEP8 validation

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,55 +1,46 @@
 #!/usr/bin/python3
-"""
-Usings API for extract information false.
-"""
-
-from sys import argv
+"""Consumimos API para extraer información ficticia"""
 import json
 import requests
+from sys import argv
 
 
 def main():
-    """
-    Query name and tasks of employee.
-    """
+    """Consultamos el nombre y las tareas de un empleado."""
 
     all_employees = {}
 
     for id in range(1, 11):
-        """
-        Use API URL to get user tasks and information
-        """
-
         url_id = f"https://jsonplaceholder.typicode.com/users/{id}"
         url_todos = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
 
         response = requests.get(url_id)
 
         if response.status_code != 200:
-            print(f"Error:username not found. Please enter an existing user id")
+            print(f"Ups... tuvimos un problema par consultar el {id}")
             exit()
 
         data = response.json()
-        EMPLOYEE_NAME = data["username"]
+        EMPLOYEE_NAME = data['username']
 
         response = requests.get(url_todos)
 
         if response.status_code != 200:
-            print(f"Error:username not found. Please enter an existing user id")
+            print(f"Ups... tuvimos un problema par consultar el {id}")
             exit()
 
         todos = response.json()
         list_todos = []
         dict_todos = {}
 
-        all_tasks = [todo["title"] for todo in todos]
-        status_task = [todo["completed"] for todo in todos]
+        all_tasks = [todo['title'] for todo in todos]
+        status_task = [todo['completed'] for todo in todos]
 
         for index in range(0, len(all_tasks)):
             dict_todos = {
-                "username": EMPLOYEE_NAME,
-                "task": all_tasks[index],
-                "completed": status_task[index]
+                'username': EMPLOYEE_NAME,
+                'task': all_tasks[index],
+                'completed': status_task[index]
             }
 
             list_todos.append(dict_todos)
@@ -58,11 +49,11 @@ def main():
 
         all_employees.update(employee_todos)
 
-        name_file_json = "todo_all_employees.json"
+        name_file_json = 'todo_all_employees.json'
 
-        with open(name_file_json, mode="w", newline='') as f:
+        with open(name_file_json, mode='w', newline='') as f:
             json.dump(all_employees, f, indent=4)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
